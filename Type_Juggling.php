233f9883b9c5bb0e4a3616dd51272b0f707fc466
qwertyasdfzxcv1234 <rsmacasio14@gmail.com>
2025-11-29 12:23:39 +0000

    Update Type_Juggling.php

--- Type_Juggling.php
+++ Type_Juggling.php
@@ -2,6 +2,15 @@
 $name = 'Rafael Macasio';
 $candies = ['Chocolate','Toffee', 'Fundge',];
 $stock = [true,false,true];
+
+//Conditional statement to check if the overall store stock is fully stocked.
+$all_in_stock = $stock[0] && $stock[1] && $stock[2];
+
+if ($all_in_stock) {
+    $store_status = "The store is fully stocked! Enjoy your visit.";
+} else {
+    $store_status = "Some candies are out of stock. Check the list below!";
+}
 ?>
 
 <!DOCTYPE html>
@@ -13,8 +22,10 @@ $stock = [true,false,true];
     <body>
         <h1> The Candy Store </h1>
         <h2>Welcome <?php echo $name; ?> </h2>
+        <p><b>Status:</b> <?php echo $store_status; ?></p>
         <p>Candies Available:<br>
             <?php
+//Loop here to print the values
             for ($i =0; $i<3; $i++) {
                 if($stock[$i]){
                     echo $candies[$i] . " is <b>in stock</b><br>";
